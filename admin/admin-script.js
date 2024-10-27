a18f0ff0ac168f213dc74d07127cd89c7f63d307
jim-hub-eng <jimg3799@gmail.com>
2024-10-26 18:30:17 -0600

    integramiento de carrusel

--- admin/admin-script.js
+++ admin/admin-script.js
@@ -24,3 +24,30 @@ function cerrarRegistrarUsuario(){ // Cuando le de click a la " x " de formulari
     //media_form.style.transition = '.3s'; // SE LE AGREGA UNA TRANSICION PARA QUE NO DESAPARESCA BRUTAMENTE
 
 }
+
+// FUNCIONES DE INPUTS DE PRODUCTOS DE HOGAR
+const nombre_hogar = document.getElementById('int-name-hogar'); // INPUT DONDE SE INGRESA EL NOMBRE DEL PRODUCTO
+const contador_nombre_hogar = document.getElementById('box-count-name-hogar'); //CAJA DONDE SE VA A ENSEÑAR EL CONTEO DE PALABRAS
+
+nombre_hogar.addEventListener('input', count_nombre_hogar); //CUANDO SE ESCRIBA EN EL INPUT VA A HACER LA FUNCION "count_nombre_hogar"
+
+function count_nombre_hogar(){
+
+    let num_letras_nombre_hogar = nombre_hogar.value.length; //LENGHT SIRVE PARA CONTAR LAS LETRAS Y SE VA ALMACENANDO EL NUMERO EN LA VARIABLE "num_letras_nombre_hogar"
+
+    contador_nombre_hogar.innerHTML = `${num_letras_nombre_hogar} / 50`; //SE MUESTRA LA CANTIDAD DE LETRAS QUE LLEVA
+
+}
+
+const des_hogar = document.getElementById('int-des-hogar'); // INPUT DONDE SE INGRESA EL NOMBRE DEL PRODUCTO
+const contador_des_hogar = document.getElementById('box-count-des-hogar'); //CAJA DONDE SE VA A ENSEÑAR EL CONTEO DE PALABRAS
+
+des_hogar.addEventListener('input', count_des_hogar); //CUANDO SE ESCRIBA EN EL INPUT VA A HACER LA FUNCION "count_nombre_hogar"
+
+function count_des_hogar(){
+
+    let num_letras_des_hogar = des_hogar.value.length; //LENGHT SIRVE PARA CONTAR LAS LETRAS Y SE VA ALMACENANDO EL NUMERO EN LA VARIABLE "num_letras_nombre_hogar"
+
+    contador_des_hogar.innerHTML = `${num_letras_des_hogar} / 230`; //SE MUESTRA LA CANTIDAD DE LETRAS QUE LLEVA
+
+}
